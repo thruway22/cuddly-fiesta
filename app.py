724a7a2daa980723ab264850df7cd9d0bd349e69
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-03 10:12:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,3 @@ conn = connect()
 
 st.title('Hello world!')
 st.write("Test 3")
-
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
