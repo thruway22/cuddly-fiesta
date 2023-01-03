e8aab44b0169865629d74600c483a5f98f6eb790
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-03 10:11:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
 import numpy as np
+from gsheetsdb import connect
+
+conn = connect()
 
 st.title('Hello world!')
 st.write("Test 3")
+
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
