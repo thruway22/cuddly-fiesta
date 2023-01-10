b74cde43425c3b920441cc35c408106b84eff3a2
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-10 03:29:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,24 +30,25 @@ st.title('SaudiREITsInfo')
 
 
 
-tickers = {
-    '4330: Riyad REIT': 4330,
-    '4331: Aljazira REIT': 4331,
-    '4332: Jadwa REIT Alharamain': 4332,
-    '4333: Taleem REIT': 4333,
-    '4334: Almaather REIT': 4334,
-    '4335: Musharaka REIT': 4335,
-    '4336: Mulkia Gulf REIT': 4336,
-    '4337: SICO Saudi REIT': 4337,
-    '4338: Alahli REIT': 4338,
-    '4339: Derayah REIT': 4339,
-    '4340: Alrajhi REIT': 4340,
-    '4342: Jadwa REIT': 4342,
-    '4344: SEDCO Capital REIT': 4344,
-    '4345: Alinma Retail REIT': 4345,
-    '4346: MEFIC REIT': 4346,
-    '4347: Bonyan REIT': 4347,
-    '4348: Alkhabeer REIT': 4348}
+tickers = {    
+    4330: '4330: Riyad REIT',
+    4331: '4331: Aljazira REIT',
+    4332: '4332: Jadwa REIT Alharamain',
+    4333: '4333: Taleem REIT',
+    4334: '4334: Almaather REIT',
+    4335: '4335: Musharaka REIT',
+    4336: '4336: Mulkia Gulf REIT',
+    4337: '4337: SICO Saudi REIT',
+    4338: '4338: Alahli REIT',
+    4339: '4339: Derayah REIT',
+    4340: '4340: Alrajhi REIT',
+    4342: '4342: Jadwa REIT',
+    4344: '4344: SEDCO Capital REIT',
+    4345: '4345: Alinma Retail REIT',
+    4346: '4346: MEFIC REIT',
+    4347: '4347: Bonyan REIT',
+    4348: '4348: Alkhabeer REIT'
+}
 
 ticker = st.selectbox('Choose a REIT fund', tickers.keys(), format_func=lambda x:tickers[x])
 
