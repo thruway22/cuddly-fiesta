b8dac213a5c1554f27de3a2a4bfd1f9eaad7f42b
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-16 05:13:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime  
 from datetime import timedelta
-
+from pandas.tseries.offsets import DateOffset
+from pandas.tseries.offsets import MonthEnd
 from utilities import *
 
 st.title('SaudiREITsInfo')
