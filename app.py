6e167d7a235702aca3e32f02c4da444ad9988cf5
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-16 06:27:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,8 @@ st.subheader('Price'+' '+u'السعر')
 st.pyplot(chart_timeseries_data(ticker, 'price'))
 
 st.header('P/FFO')
-st.subheader(components.html("""<div dir="rtl">مكرر النقد من العمليات</div>"""))
+components.html("""<div dir="rtl">مكرر النقد من العمليات</div>""")
+#st.subheader()
 
 
 st.pyplot(chart_timeseries_data(ticker, 'pffo'))
