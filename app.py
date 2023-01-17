4ff99dd87a619a31d2b2d20682c85b10885e200d
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-17 14:15:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,27 +13,6 @@ from utilities import *
 
 # st.set_page_config(layout="wide")
 
-'''style_fullscreen_button_css = """
-    button[title="View fullscreen"] {
-        background-color: #004170cc;
-        right: 0;
-        color: white;
-        
-    }
-
-    button[title="View fullscreen"]:hover {
-        background-color:  #004170;
-        color: white;
-        visibility: hidden;
-        }
-    """
-st.markdown(
-    "<style>"
-    + style_fullscreen_button_css
-    + "</styles>",
-    unsafe_allow_html=True,
-)'''
-
 st.markdown('''<style> button[title="View fullscreen"] {visibility: hidden;}
             button[title="View fullscreen"]:hover {visibility: hidden;}</styles>''',
             unsafe_allow_html=True)
