d4b1a751a14b8b662e9ae4794010f6fce0096b55
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-18 15:14:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,6 +76,21 @@ else:
 
         with col1b:
             st.markdown('<hr />', unsafe_allow_html=True)
+            st.write('net_debt_ebitda')
+            st.markdown(display_text(
+                body=texts.loc['ffos_body'].value, title=texts.loc['ffos_title'].value), 
+                        unsafe_allow_html=True)
+            st.pyplot(chart_categorical_data(ticker, 'net_debt_ebitda'))
+                
+            st.markdown('<hr />', unsafe_allow_html=True)
+            st.write('coverage')
+            st.markdown(display_text(
+                body=texts.loc['ffo_payout_body'].value, title=texts.loc['ffo_payout_title'].value), 
+                        unsafe_allow_html=True)
+            st.pyplot(chart_categorical_data(ticker, 'coverage'))
+            
+        with col1b:
+            st.markdown('<hr />', unsafe_allow_html=True)
             st.markdown(display_text(
                 body=texts.loc['ffos_body'].value, title=texts.loc['ffos_title'].value), 
                         unsafe_allow_html=True)
