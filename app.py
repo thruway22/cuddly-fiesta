f1ba3f11cc8b248eefad55e5bfbb93f20c880dda
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-22 20:50:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def display_text(header=None, body=None, title=None, **extra_bodies):
     output = ''
     
     if title != None:
-        output = output + f'<p id="title">{texts.loc[title].value}</p>'
+        output = output + f'<t id="title">{texts.loc[title].value}</t>'
 
     if header != None:
         output = output + f'<p id="header">{texts.loc[header].value}</p>'
