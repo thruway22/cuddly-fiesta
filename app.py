608e7ea7acd4a20976fbc12667901dfd93e14e03
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-28 02:53:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ tickers = {
 }
 
 pdata = pd.read_csv('data/pdata.csv')
-st.dataframe(pdata['date'].tail(1)[0])
+st.write(pdata['date'].tail(1)[0])
 pdata['date'] = pd.to_datetime(pdata['date'], format='%Y-%m-%d')
 
 fdata = pd.read_csv('data/fdata.csv')
